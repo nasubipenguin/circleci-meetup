7eec5115f9292d9277cfe132e98b2adc06856917
nasubipenguin <nasubipenguin@users.noreply.github.com>
2018-06-06 23:38:00 +0900

    Fix indent from tab to space

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const fizzBuzz = function(n) {
-	// But, we want to return 'Fizz Buzz' if n is multiples of 3 and multiples of 5
-	if (n % 3 === 0 && n % 5 === 0) {
-		return "Fizz Buzz";
-	}
+  // But, we want to return 'Fizz Buzz' if n is multiples of 3 and multiples of 5
+  if (n % 3 === 0 && n % 5 === 0) {
+    return "Fizz Buzz";
+  }
 
   // return 'Fizz' if n is multiples of 3
   if (n % 3 === 0) {
